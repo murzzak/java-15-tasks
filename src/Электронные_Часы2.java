905904bd8fe18a50901c12db5a72cff1c1d855b4
Elhan <elhanzanibaev@gmail.com>
2026-09-23 22:49:32 +0600

    Задача сделана

---
+++ src/Электронные_Часы2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Электронные_Часы2 {
+    public static void main(String[] args) {
+        Scanner vvod = new Scanner(System.in);
+        int n = vvod.nextInt();
+        int h = (n / 3600) % 24;
+        int m = (n % 3600) / 60;
+        int s = n % 60;
+        System.out.printf("%d:%02d:%02d\n", h, m, s);
+    }
+}
