fa51b211e7bfb7c5c22896c81ee1e3854a203ee1
Elhan <elhanzanibaev@gmail.com>
2026-09-23 23:09:23 +0600

    Задача сделана

--- src/Конец_Уроков.java
+++ src/Конец_Уроков.java
@@ -3,14 +3,20 @@ public class Конец_Уроков {
     public static void main(String[] args) {
         Scanner vvod = new Scanner(System.in);
         int n = vvod.nextInt();
-        int totalMinutes = n * 45;
-        for (int i = 1; i < n; i++) {
-            if (i % 2 == 0) {
-                totalMinutes += 15;
-            } else {
-                totalMinutes += 5;
-            }
+        int breaks = n - 1;
+        int shortBreaks;
+        int longBreaks;
+        if (breaks % 2 == 0) {
+            shortBreaks = breaks / 2;
+            longBreaks = breaks / 2;
+
+        } else {
+            shortBreaks = breaks / 2 + 1;
+            longBreaks = breaks / 2;
+
         }
+
+        int totalMinutes = n * 45 + shortBreaks * 5 + longBreaks * 15;
         int hours = 9 + totalMinutes / 60;
         int minutes = totalMinutes % 60;
         System.out.println(hours + " " + minutes);
