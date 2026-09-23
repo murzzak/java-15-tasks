5c99c982ac365bb5e190af4f4c462a80b5988810
Elhan <elhanzanibaev@gmail.com>
2026-09-23 21:52:27 +0600

    Задача сделана

--- src/Hypotenuse.java
+++ src/Hypotenuse.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
 public class Hypotenuse {
+    public static void main(String[] args) {
+        Scanner vvod = new Scanner(System.in);
+        double a = vvod.nextDouble();
+        double b = vvod.nextDouble();
+        double c = Math.sqrt(a*a + b*b);
+        System.out.println(c);
+    }
 }
