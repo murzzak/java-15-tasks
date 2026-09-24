797f3334dec118760a802c88fe8e25e636fcd92d
Elhan <elhanzanibaev@gmail.com>
2026-09-24 08:18:21 +0600

    Задача сделана

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,1 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner vvod = new Scanner(System.in);
-        int a = vvod.nextInt();
-        int ed = a % 10;
-        int sot = a / 100;
-        int des = (a / 10) % 10;
-        System.out.println(ed+sot+des);
-    }
-}
+public
